d0c81b052c591ae2fff998cb42f500b1b786f533
ukbarr <ukbarr@pm.me>
2023-12-08 00:00:10 +0100

    yy

--- config/keymap_french_mac_iso.h
+++ config/keymap_french_mac_iso.h
@@ -8,13 +8,13 @@
 // clang-format off
 
 // Aliases
-#define FRM_AT   N2  // @
+#define FRM_AT   RA(N2)  // @
 #define FRM_EXLM N8    // !
 #define FRM_MINS EQUAL  // -
-#define FRM_GRV  N3 // `
+#define FRM_GRV  RA(N3) // `
 #define FRM_LABK GRAVE // <
 #define FRM_EQL  SLASH // =
-#define FRM_HASH  HASH  // #
+#define FRM_HASH HASH  // #
 #define FRM_UNDS LS(EQUAL) // _
 #define FRM_RABK LS(GRAVE) // >
 #define FRM_PLUS LS(SLASH)  // +
@@ -23,6 +23,6 @@
 #define FRM_EURO RA(RBKT)  // €
 #define FRM_TILD RA(FR_N)    // ~ (dead)
 #define FRM_LBRC LS(RA(N5)) // [
-#define FRM_RBRC N7 // ]
+#define FRM_RBRC RA(N7) // ]
 #define FRM_PIPE LS(RA(FR_L))    // |
 #define FRM_BSLS LS(RA(FR_COLN)) // (backslash)
